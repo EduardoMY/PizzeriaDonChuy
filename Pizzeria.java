0f6de5d7e1edc9ca080ecb543b302a39517b7da9
ALexis Poo Montenegro <apoomo@hotmail.com>
2016-12-11 22:30:21 -0800

    Main menu

--- Pizzeria.java
+++ Pizzeria.java
@@ -1,5 +1,42 @@
-class Pizzeria{
-    public static void main(String[] args){
-	System.out.println("Hola vivero");
+import java.io.*;
+import java.util.*;
+import java.util.Scanner;
+public class Pizzeria
+{
+    
+    public static void main(String[] args) throws IOException {
+    
+		Scanner in=new Scanner(System.in);
+		int option;
+		do
+		{
+			System.out.print("Bienvenidos a pizzeria Don Chuy\n");
+			System.out.print("Seleccione una opción\n");
+			System.out.print("1) Ordenar una pizza\n");
+			System.out.print("2) Ordenar una soda\n");
+			System.out.print("3) Ordenar una ensalada\n");
+			System.out.print("4) Total del pedido\n");
+			System.out.print("5)Salir\n");
+			option=in.nextInt();
+		
+			switch(option)
+			{
+			case 1:
+					//pizza();
+			break;
+
+			case 2:
+					//soda();
+			break;
+			case 3:
+					//ensalada();
+			break;
+			case 4:
+					//total();
+			break;
+			}
+
+		  }while(option!=5);
     }
+
 }
